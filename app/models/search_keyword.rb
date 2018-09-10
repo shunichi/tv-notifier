9c581783a4781c8f7f90475a93fcaea3b4022f2f
Shunichi Ikegami <sike.tm@gmail.com>
2018-09-10 11:52:55 +0900

    検索結果が多いときは少しずつ送信

--- app/models/search_keyword.rb
+++ app/models/search_keyword.rb
@@ -3,7 +3,8 @@ class SearchKeyword < ApplicationRecord
   has_many :tv_programs, dependent: :nullify
   validates :keyword, presence: true
 
-  NOTIFICATION_ITEM_MAX = 10
+  NOTIFICATION_ITEM_MAX = 20
+  NOTIFICATION_ITEMS_PER_MESSAGE = 5
 
   def search_and_notify
     items = TvProgram::Collector.search(keyword)
@@ -11,6 +12,23 @@ class SearchKeyword < ApplicationRecord
     items = items.reject { |item| urls.member?(item.link) }
     items = items.take(NOTIFICATION_ITEM_MAX)
     return if items.blank?
+    items.each_slice(NOTIFICATION_ITEMS_PER_MESSAGE).each do |subitems|
+      item_messages = subitems.map do |item|
+        <<~EOS
+        ■ #{item.title}
+        #{item.description}
+        #{item.link}
+        EOS
+      end
+      message = %(「#{keyword}」に関連する番組が見つかりました！\n\n#{item_messages.join("\n")})
+      if line_notification_target = user.line_notification_targets.first
+        Line::Notify.new(line_notification_target.token).send(message)
+      end
+      create_tv_programs(subitems)
+    end
+  end
+
+  def create_tv_programs(items)
     TvProgram.transaction do
       items.each do |item|
         tv_programs.create!(
@@ -22,17 +40,6 @@ class SearchKeyword < ApplicationRecord
           description: item.description
         )
       end
-    end
-    item_messages = items.map do |item|
-      <<~EOS
-      ■ #{item.title}
-      #{item.description}
-      #{item.link}
-      EOS
-    end
-    message = %(「#{keyword}」に関連する番組が見つかりました！\n\n#{item_messages.join("\n")})
-    if line_notification_target = user.line_notification_targets.first
-      Line::Notify.new(line_notification_target.token).send(message)
     end
   end
 end
